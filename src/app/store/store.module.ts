bec4d5b319c2e8c1c8de0874898bb8150f19e8d3
Jonas Munsch <jonas1.munsch@web.de>
2020-01-06 18:54:00 +0100

    import material module to store

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { StoreRoutingModule } from './store-routing.module';
-import { StoreComponent } from './store.component';
-
+import { StoreRoutingModule } from "./store-routing.module";
+import { StoreComponent } from "./store.component";
+import { AppMaterialModule } from "../app-material/app-material.module";
 
 @NgModule({
   declarations: [StoreComponent],
-  imports: [
-    CommonModule,
-    StoreRoutingModule
-  ]
+  imports: [CommonModule, StoreRoutingModule, AppMaterialModule]
 })
-export class StoreModule { }
+export class StoreModule {}
