958ed5bf0f8f630982ab1d526b79862d57aa1090
Jonas Munsch <jonas1.munsch@web.de>
2020-01-08 22:53:39 +0100

    import Material Modules as AppMaterialModule

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ProjectsRoutingModule } from './projects-routing.module';
-import { ProjectsComponent } from './projects.component';
-
+import { ProjectsRoutingModule } from "./projects-routing.module";
+import { ProjectsComponent } from "./projects.component";
+import { AppMaterialModule } from "../app-material/app-material.module";
 
 @NgModule({
   declarations: [ProjectsComponent],
-  imports: [
-    CommonModule,
-    ProjectsRoutingModule
-  ]
+  imports: [CommonModule, ProjectsRoutingModule, AppMaterialModule]
 })
-export class ProjectsModule { }
+export class ProjectsModule {}
