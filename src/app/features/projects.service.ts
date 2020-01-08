caeff5128159db60647148f4f4487243cac53185
Jonas Munsch <jonas1.munsch@web.de>
2020-01-08 23:07:33 +0100

    add get request to fetch from httpclient

--- src/app/features/projects.service.ts
+++ src/app/features/projects.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+const BASE_URL = `http://localhost:3000/`;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ProjectsService {
+  model = "projects";
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getProjects(): Observable<any> {
+    return this.httpClient.get<any>(`${BASE_URL}${this.model}`);
+  }
 }
