ca418560dec8dcfbcaa6a9499d32dbfd8340c7d2
jsmunsch <54038089+jsmunsch@users.noreply.github.com>
2020-01-09 21:00:20 +0100

    Projects actions (#11)
    * add create, update and delete actions to projects service
    
    * add json server script
    
    * finalize delete project function
    
    * add form material module
    
    * bind form via ngModel and import FormsModule, resetProject as fallback on init
    
    * add forms for details and percentComplete and bind them to properties
    
    * use store service to update,create and save a project

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ProjectsService } from "../features/projects.service";
+import { ThrowStmt } from "@angular/compiler";
+import { Projects } from "../features/projects";
+import { stringify } from "querystring";
 
 @Component({
   selector: "app-projects",
@@ -9,12 +12,13 @@ import { ProjectsService } from "../features/projects.service";
 export class ProjectsComponent implements OnInit {
   primaryColor = "red";
   projects$;
-  selectedProject;
+  selectedProject: Projects;
 
   constructor(private ProjectsService: ProjectsService) {}
 
   ngOnInit() {
     this.getProjects();
+    this.resetProject();
   }
 
   getProjects() {
@@ -27,7 +31,45 @@ export class ProjectsComponent implements OnInit {
     this.selectedProject = project;
   }
 
+  resetProject() {
+    const emptyProject: Projects = {
+      id: null,
+      title: "",
+      details: "",
+      percentComplete: 0,
+      approved: false
+    };
+    this.selectProject(emptyProject);
+  }
+
+  saveProject(project) {
+    if (!project.id) {
+      this.createProject(project);
+    } else {
+      this.updateProject(project);
+    }
+  }
+
+  deleteProject(project) {
+    this.ProjectsService.delete(project.id).subscribe(() => this.getProjects());
+  }
+
+  createProject(project) {
+    this.ProjectsService.create(project).subscribe(() => {
+      this.getProjects();
+      this.resetProject();
+    });
+  }
+
+  updateProject(project) {
+    this.ProjectsService.update(project).subscribe(() => {
+      this.getProjects();
+      this.resetProject();
+    });
+  }
+
   cancel() {
     this.selectProject(null);
+    this.resetProject();
   }
 }
