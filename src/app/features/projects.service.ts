4542edf77bd08da162c757445395990eb98addd7
Jonas Munsch <jonas1.munsch@web.de>
2020-01-09 19:10:19 +0100

    add create, update and delete actions to projects service

--- src/app/features/projects.service.ts
+++ src/app/features/projects.service.ts
@@ -12,7 +12,26 @@ export class ProjectsService {
   model = "projects";
   constructor(private httpClient: HttpClient) {}
 
+  getUrl() {
+    return `${BASE_URL}${this.model}`;
+  }
+
+  getUrlForId(id) {
+    return `${this.getUrl()}/${id}`;
+  }
   all(): Observable<Projects> {
-    return this.httpClient.get<Projects>(`${BASE_URL}${this.model}`);
+    return this.httpClient.get<Projects>(this.getUrl());
+  }
+
+  create(project) {
+    return this.httpClient.post(this.getUrl(), project);
+  }
+
+  update(project) {
+    return this.httpClient.patch(this.getUrlForId(project.id), project);
+  }
+
+  delete(projectId) {
+    return this.httpClient.delete(this.getUrlForId(projectId));
   }
 }
