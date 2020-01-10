7243e6603861f6d347b5ec645573254e25d16f64
Jonas Munsch <jonas1.munsch@web.de>
2020-01-10 17:39:17 +0100

    isolate project title

--- src/app/projects/project-view/project-view.component.ts
+++ src/app/projects/project-view/project-view.component.ts
@@ -8,7 +8,9 @@ import { Projects } from "src/app/features/projects";
 })
 export class ProjectViewComponent implements OnInit {
   currentProject: Projects;
+  originalTitle;
   @Input() set project(value) {
+    if (value) this.originalTitle = value.title;
     this.currentProject = Object.assign({}, value);
   }
 
