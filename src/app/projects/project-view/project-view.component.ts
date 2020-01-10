215650eada64954f37536a635c17ab04b36f1395
Jonas Munsch <jonas1.munsch@web.de>
2020-01-10 17:29:39 +0100

    add input and output decorators for view component

--- src/app/projects/project-view/project-view.component.ts
+++ src/app/projects/project-view/project-view.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Projects } from "src/app/features/projects";
 
 @Component({
-  selector: 'app-project-view',
-  templateUrl: './project-view.component.html',
-  styleUrls: ['./project-view.component.scss']
+  selector: "app-project-view",
+  templateUrl: "./project-view.component.html",
+  styleUrls: ["./project-view.component.scss"]
 })
 export class ProjectViewComponent implements OnInit {
+  @Input() project: Projects;
+  @Output() saved = new EventEmitter();
+  @Output() cancelled = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
